9829107868c23399ca8db11c672f5dedc8af9933
Jessie <FranceJessieJ@gmail.com>
2020-05-12 12:57:01 -0600

    Added Editor, Previewer, and App as new react components.

--- src/js/components/MarkdownPreviewer.js
+++ src/js/components/MarkdownPreviewer.js
@@ -1,44 +1,85 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-class Form extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      value: ""
-    };
+function PageTitle() {
+    return  <h1>Markdown Previewer</h1>;
+}
 
+
+class Editor extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+	  editorText: 'Will enter markdown text here...'
+      };
     this.handleChange = this.handleChange.bind(this);
+      
   }
 
   handleChange(event) {
-    const { value } = event.target;
-    this.setState(() => {
-      return {
-        value
-      };
-    });
+    this.setState({editorText: event.target.value});
   }
-
+    
   render() {
       return (
-    <div>
-       <h1>Markdown Previewer</h1>
-      <form>
-        <textarea
-          type="text"
-	  id="editor"
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
+	  <div>
+	      <h2>Editor</h2>
+              <form>
+              <textarea
+	  id = "editor"
+	  type="text"
+	  value={this.state.editorText}
+	  onChange={this.handleChange} />
 	      </form>
-</div>
+	      </div>
     );
   }
 }
 
-export default Form;
+
+class Previewer extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+	  markdownText: 'Results will appear here...'
+      };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({markdownText: event.target.value});
+  }
+
+  render() {
+      return (
+	  <div>
+	      <h2>Previewer</h2>
+              <div id="preview">{this.state.markdownText}</div>
+	  </div>
+    );
+  }
+}
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+    
+  render() {
+      return (
+	  <div>
+              <PageTitle />
+	      <Editor />
+	      <Previewer />
+	  </div>
+      )
+  }
+}
+
+export default App;
 
 const wrapper = document.getElementById("root");
-wrapper ? ReactDOM.render(<Form />, wrapper) : false;
+wrapper ? ReactDOM.render(<App />, wrapper) : false;

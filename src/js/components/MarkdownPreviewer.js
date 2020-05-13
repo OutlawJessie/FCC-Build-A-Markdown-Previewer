44755b9d841d548156143e65945b084c137451a9
Jessie <FranceJessieJ@gmail.com>
2020-05-13 15:45:54 -0600

    Added marked.js library for markdown conversion and initialized markdown for FCC tests.

--- src/js/components/MarkdownPreviewer.js
+++ src/js/components/MarkdownPreviewer.js
@@ -1,6 +1,43 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+// For the marked.js library, allow line breaks.
+// This achieves the optional challenge in the FCC guidelines.
+marked.setOptions({
+  breaks: true,
+});
+
+// Create initial markdown text.
+const initialEditorText = `
+# This is markdown as h1 heading.
+
+## This is markdown as h2 heading.
+
+Here are some span tags inline code \`<span></span>\` between back ticks.
+This text will appear **bold** with asterisks.
+> This is a block quote.
+This is a link to the marked.js documentation [links](https://marked.js.org/#/README.md#README.md)
+
+\`\`\`
+// This is a code block
+
+function sayHello(firstLine, lastLine) {
+    console.log("hello!");
+}
+\`\`\`
+  
+- This is a list
+  - With items.
+  - and sub-item
+And this is an embedded image of the React logo:						      
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
+
+
+
+
+
+
 
 function PageTitle() {
     return  <h1>Markdown Previewer</h1>;
@@ -15,8 +52,8 @@ function Editor(props) {
               <textarea
 	        id = "editor"
 	        type="text"
-	        value={props.editorText}//{this.state.editorText}
-	        onChange={props.handleChange}//onChange={this.handleChange}
+	        value={props.editorText}
+	        onChange={props.handleChange}
 	      />
 	      </form>
 	  </div>
@@ -28,8 +65,12 @@ function Previewer(props) {
       return (
 	  <div>
 	      <h2>Previewer</h2>
-              <div id="preview">{props.markdownText}</div>	      
-	  </div>
+	      {/*Call React dangerouslySetInnerHTML function*/}
+        <div 
+          id="preview"
+          dangerouslySetInnerHTML = {{ __html: props.markdownText }}
+	      />
+       </div>
     );
 }
 
@@ -39,8 +80,8 @@ class App extends Component {
   constructor(props) {
       super(props);
       this.state = {
-	  editorText: 'Will enter markdown text here...',
-	  markdownText: 'Results will appear here...'
+	  editorText: initialEditorText,
+	  markdownText: marked(initialEditorText)
       };      
     this.handleChange = this.handleChange.bind(this);      
   }
@@ -49,18 +90,18 @@ class App extends Component {
 
       this.setState({
 	  editorText: event.target.value,
-	  markdownText: this.state.editorText
-      }); // TODO: need to fix lag in upadate for markdownText in Previewer function
+	  markdownText: marked(event.target.value)
+      }); 
 
 
     }
     
-  render() {
+    render() {
       return (
 	  <div>
               <PageTitle />
 	      <Editor editorText={this.state.editorText} handleChange={this.handleChange}/>	      
-	      <Previewer markdownText={this.state.markdownText}  />
+	      <Previewer markdownText={this.state.markdownText} handleChange={this.handleChange} />
 	  </div>
       )
   }
